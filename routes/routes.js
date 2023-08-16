e2847bff6807c88212150a4705f0a8c2858dcb1c
Nazif <nazif21@DESKTOP-BBNEEII.localdomain>
2023-08-16 14:35:39 +0600

    Initial commit.

---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res) => res.send("This is a get request."))
+router.post("/", (req, res) => res.send("This is a post request"))
+router.patch("/", (req, res) => res.send("This is a patch request"))
+router.put("/", (req, res) => res.send("This is a put request"))
+router.delete("/", (req, res) => res.send("This is a delete request"))
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = require("./routes/routes")
+const PORT = process.env.port || 2000
+const app = express()
+
+app.use("/", router)
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
